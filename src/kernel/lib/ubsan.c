ed13b1b54920a23b7de3de3c9190fa56d360fbc9
Iurii Zamiatin <umz38@yandex.ru>
2021-09-16 15:16:46 +0300

    ubsan: Call panic_handler directly

--- src/kernel/lib/ubsan.c
+++ src/kernel/lib/ubsan.c
@@ -65,80 +65,69 @@ struct ubsan_invalid_builtin_data {
 attribute_noinline void __ubsan_handle_type_mismatch_v1(struct ubsan_mismatch_data *data,
                                                         uintptr_t ptr) {
 	if (ptr == 0) {
-		LOG_PANIC(UBSAN_LOC_FMT " null dereference", UBSAN_FROM_LOC(data->location));
+		panic_handler("ubsan", UBSAN_LOC_FMT " null dereference", UBSAN_FROM_LOC(data->location));
 	} else if (data->align != 0 && align_up(ptr, 1 << data->align) != ptr) {
-		LOG_PANIC(UBSAN_LOC_FMT " pointer %p is not aligned to %d", UBSAN_FROM_LOC(data->location),
-		          ptr, 1 << data->align);
+		panic_handler("ubsan", UBSAN_LOC_FMT " pointer %p is not aligned to %d",
+		              UBSAN_FROM_LOC(data->location), ptr, 1 << data->align);
 	} else {
-		LOG_PANIC(UBSAN_LOC_FMT
-		          " pointer %p points to a cell that is not big enough to store \"%s\"",
-		          UBSAN_FROM_LOC(data->location), ptr, data->type->name);
+		panic_handler("ubsan",
+		              UBSAN_LOC_FMT
+		              " pointer %p points to a cell that is not big enough to store \"%s\"",
+		              UBSAN_FROM_LOC(data->location), ptr, data->type->name);
 	}
-	hang();
 }
 
 attribute_noinline void __ubsan_handle_add_overflow() {
-	LOG_PANIC("addition overflow");
-	hang();
+	panic_handler("ubsan", "addition overflow");
 }
 
 attribute_noinline void __ubsan_handle_sub_overflow() {
-	LOG_PANIC("substraction overflow");
-	hang();
+	panic_handler("ubsan", "substraction overflow");
 }
 
 attribute_noinline void __ubsan_handle_mul_overflow() {
-	LOG_PANIC("multiplication overflow");
-	hang();
+	panic_handler("ubsan", "multiplication overflow");
 }
 
 attribute_noinline void __ubsan_handle_divrem_overflow() {
-	LOG_PANIC("division overflow");
-	hang();
+	panic_handler("ubsan", "division overflow");
 }
 
 attribute_noinline void __ubsan_handle_negate_overflow() {
-	LOG_PANIC("negate overflow");
-	hang();
+	panic_handler("ubsan", "negate overflow");
 }
 
 attribute_noinline void __ubsan_handle_load_invalid_value(void *data_raw) {
 	struct ubsan_invalid_value_data *data = (struct ubsan_invalid_value_data *)data_raw;
-	LOG_PANIC(UBSAN_LOC_FMT " load of invalid value (type \"%s\")", UBSAN_FROM_LOC(data->location),
-	          data->type->name);
-	hang();
+	panic_handler("ubsan", UBSAN_LOC_FMT " load of invalid value (type \"%s\")",
+	              UBSAN_FROM_LOC(data->location), data->type->name);
 }
 
 attribute_noinline void __ubsan_handle_pointer_overflow(void *data_raw, void *lhs, void *rhs) {
 	struct ubsan_overflow_data *data = (struct ubsan_overflow_data *)data_raw;
-	LOG_PANIC(UBSAN_LOC_FMT " pointer overflow with operands %p, %p",
-	          UBSAN_FROM_LOC(data->location), lhs, rhs);
-	hang();
+	panic_handler("ubsan", UBSAN_LOC_FMT " pointer overflow with operands %p, %p",
+	              UBSAN_FROM_LOC(data->location), lhs, rhs);
 }
 
 attribute_noinline void __ubsan_handle_shift_out_of_bounds(void *data_raw, void *lhs, void *rhs) {
 	struct ubsan_out_of_bounds_data *data = (struct ubsan_out_of_bounds_data *)data_raw;
-	LOG_PANIC(UBSAN_LOC_FMT " shift out of bounds %U << %U", UBSAN_FROM_LOC(data->location), lhs,
-	          rhs);
-	hang();
+	panic_handler("ubsan", UBSAN_LOC_FMT " shift out of bounds %U << %U",
+	              UBSAN_FROM_LOC(data->location), lhs, rhs);
 }
 
 attribute_noinline void __ubsan_handle_out_of_bounds(void *data_raw, void *index) {
 	struct ubsan_out_of_bounds_data *data = (struct ubsan_out_of_bounds_data *)data_raw;
-	LOG_PANIC(UBSAN_LOC_FMT " out of bounds at index %d", UBSAN_FROM_LOC(data->location),
-	          (intptr_t)index);
-	hang();
+	panic_handler("ubsan", UBSAN_LOC_FMT " out of bounds at index %d",
+	              UBSAN_FROM_LOC(data->location), (intptr_t)index);
 }
 
 attribute_noinline void __ubsan_handle_vla_bound_not_positive(void *data_raw, void *bound) {
 	struct ubsan_vla_bound_data *data = (struct ubsan_vla_bound_data *)data_raw;
-	LOG_PANIC(UBSAN_LOC_FMT " vla bound %p not positive", UBSAN_FROM_LOC(data->location),
-	          (intptr_t)bound);
-	hang();
+	panic_handler("ubsan", UBSAN_LOC_FMT " vla bound %p not positive",
+	              UBSAN_FROM_LOC(data->location), (intptr_t)bound);
 }
 
 attribute_noinline void __ubsan_handle_invalid_builtin(void *data_raw) {
 	struct ubsan_invalid_builtin_data *data = (struct ubsan_invalid_builtin_data *)data_raw;
-	LOG_PANIC(UBSAN_LOC_FMT " invalid builtin", UBSAN_FROM_LOC(data->location));
-	hang();
+	panic_handler("ubsan", UBSAN_LOC_FMT " invalid builtin", UBSAN_FROM_LOC(data->location));
 }
